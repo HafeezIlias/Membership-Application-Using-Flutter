758ccbbdeffc040390a56251ca54076af1f23915
HafeezIlias <HafeezIlias.1@gmail.com>
2025-01-09 21:29:04 +0800

    add membership and user model

---
+++ lib/models/user.dart
@@ -0,0 +1,48 @@
+class User {
+  String? userid;
+  String? useremail;
+  String? username;
+  String? userphone;
+  String? userpassword;
+  String? userdatereg;
+  String? useraddress;
+  String? userprofileimage;
+  String? userranking;
+
+  User(
+      {this.userid,
+      this.useremail,
+      this.username,
+      this.userphone,
+      this.userpassword,
+      this.userdatereg,
+      this.useraddress,
+      this.userprofileimage,
+      this.userranking});
+
+  User.fromJson(Map<String, dynamic> json) {
+    userid = json['userid'];
+    useremail = json['useremail'];
+    username = json['username'];
+    userphone = json['userphone'];
+    userpassword = json['userpassword'];
+    userdatereg = json['userdatereg'];
+    useraddress = json['useraddress'];
+    userprofileimage = json['userprofileimage'];
+    userranking = json['userranking'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['userid'] = userid;
+    data['useremail'] = useremail;
+    data['username'] = username;
+    data['userphone'] = userphone;
+    data['userpassword'] = userpassword;
+    data['userdatereg'] = userdatereg;
+    data['useraddress'] = useraddress;
+    data['userprofileimage'] = userprofileimage;
+    data['userranking'] = userranking;
+    return data;
+  }
+}
